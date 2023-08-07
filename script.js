2f311a4937acb54d5624458468460f6ce41bd79f
HappyGames <harkaransingh528@gmail.com>
2023-08-07 21:35:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+gsap.from(".navbar", {
+    y: -100,
+    duration: 1.1,
+})
+
+gsap.from(".image-1",{
+    x: -70,
+    duration: 1,
+})
+
+gsap.from(".image-2",{
+    x: 70,
+    duration: 1,
+})
+
+gsap.from(".image-3",{
+    y: 50,
+    duration: 1,
+})
+
+gsap.from(".image-4",{
+    y: 50,
+    duration: 1,
+})
+
+gsap.from(".heading h1", {
+    y: 200,
+    duration: 1,
+})
